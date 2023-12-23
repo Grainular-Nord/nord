0f4f0ef8286752ab477c80856ca7a3ea95f89477
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-12-24 00:22:04 +0100

    fix: remove grainId from type

--- src/types/dev-grain.d.ts
+++ src/types/dev-grain.d.ts
@@ -2,4 +2,4 @@
 
 import { Grain } from '../../dist/types';
 
-export type DevGrain = Grain<any> & { isGrain: true; grainId: ReturnType<Crypto['randomUUID']> };
+export type DevGrain = Grain<any> & { isGrain: true };
