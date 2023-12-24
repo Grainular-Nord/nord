cb5eb13fc52307129c42487b33d733283d77c703
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-12-24 17:48:00 +0100

    refactor: change replacement mechanism in If directive

--- src/lib/directives/if.ts
+++ src/lib/directives/if.ts
@@ -9,21 +9,17 @@ export const If = <T>(value: T | ReadonlyGrain<T>, run: (value: T) => NodeList) 
     // Return the created template directive
     const ifFunc = (node: Text) => {
         const initialValue: T = isGrain(value) ? value() : value;
-        const template = [...run(initialValue)];
-        node.replaceWith(...template);
+        const [template] = [...run(initialValue)];
+        node.replaceWith(...[template]);
 
         if (isGrain(value)) {
-            // Get the common ancestor for all nodes. If there is more then one ancestor, something went very wrong
-            const [root, ...rest] = [...new Set(template.map((node) => node.parentElement))];
-            console.log({ root });
-            if (rest.length !== 0 || !root) throw new TypeError(Error.MULTIPLE_ROOTS, { cause: 'NodeListComparison' });
-
+            let currentElement: Node | undefined = template;
             // Subscribe to the grain and equalize the nodeLists whenever the value changes
             value.subscribe((value: T) => {
-                const template = [...run(value)];
-                øEqualizeNodeLists(root, template);
+                const [replace] = [...run(value)];
+                currentElement?.parentElement?.replaceChild(replace, currentElement);
+                currentElement = replace;
             });
-            return;
         }
     };
 
