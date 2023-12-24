65e7f7647a55254564749e990be9ecc384f3af59
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-12-24 19:45:50 +0100

    fix: resolve issue with empty list edgecase

--- src/lib/directives/for-each.ts
+++ src/lib/directives/for-each.ts
@@ -9,7 +9,11 @@ export const ForEach = <T>(value: T[] | ReadonlyGrain<T[]>, run: (elem: T, index
     const forEach = (node: Text) => {
         // Render the initial list
         const initialValue: T[] = isGrain(value) ? value() : value;
-        const list = initialValue.flatMap((value, index) => [...run(value, index)]);
+        let list = initialValue.flatMap((value, index) => [...run(value, index)]);
+        if (list.length === 0) {
+            list = [document.createTextNode('')];
+        }
+
         node.replaceWith(...list);
 
         // If the value is a grain, subscriptions need to be setup to render the
@@ -17,6 +21,7 @@ export const ForEach = <T>(value: T[] | ReadonlyGrain<T[]>, run: (elem: T, index
         if (isGrain(value)) {
             // Get the common ancestor for all nodes. If there is more then one ancestor, something went very wrong
             const [root, ...rest] = [...new Set(list.map((node) => node.parentElement))];
+            console.log({ list, root, rest });
             if (rest.length !== 0 || !root) throw new TypeError(Error.MULTIPLE_ROOTS, { cause: 'NodeListComparison' });
 
             // Subscribe to the grain and equalize the nodeLists whenever the value changes
