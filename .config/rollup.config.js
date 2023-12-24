0ec672629e044e567677749c1e1224b3fe7a3af4
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-12-24 09:21:10 +0100

    feat: add minification support for cdn bundle

--- .config/rollup.config.js
+++ .config/rollup.config.js
@@ -6,6 +6,7 @@ import cleanup from 'rollup-plugin-cleanup';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import pkg from '../package.json' assert { type: 'json' };
+import terser from '@rollup/plugin-terser';
 
 const bundle = (config) => ({
     input: './src/index.ts',
@@ -26,6 +27,17 @@ export default [
                 file: pkg.module,
                 format: 'es',
                 sourcemap: true,
+            },
+            /**
+             * The IIFE bundle is used for delivery via unpgk CDN and as the browser package file. It will work in any
+             * browser, even if it doesn't support ESM Modules, imports or exports.
+             */
+            {
+                file: pkg.unpkg,
+                format: 'iife',
+                sourcemap: true,
+                plugins: [terser()],
+                name: 'nord',
             },
         ],
     }),
