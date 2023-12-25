11a24f043d8daf347319851e23dd8ffdd8d56898
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-12-25 17:46:56 +0100

    feat: add context property to namespace

--- src/lib/create-namespace.ts
+++ src/lib/create-namespace.ts
@@ -6,6 +6,7 @@ export const createNamespace = () => {
     if (!window.$$nord) {
         window.$$nord = {
             // Create a default namespace
+            context: new Map(),
             directives: new Map<string, DirectiveHandler<Text> | DirectiveHandler<Element>>(),
         };
     }
