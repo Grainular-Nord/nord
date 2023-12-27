32fb2bdcc519d927f51b94ed66d245e5dd1b72bf
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-12-27 23:58:46 +0100

    feat: api improvements for derived and combined grains

--- src/lib/grains/combined.ts
+++ src/lib/grains/combined.ts
@@ -1,5 +1,6 @@
 /** @format */
 
+import { Subscriber } from '../../../dist/types';
 import { ReadonlyGrain } from '../../types';
 import { GrainValue } from '../../types/grain-value';
 import { grain } from './grain';
@@ -11,54 +12,87 @@ import { readonly } from './readonly';
  * The new grain's value is calculated using a provided function, which takes the current values
  * of all dependent grains as input.
  *
+ * The combined grain sets up its subscriptions to the dependent grains lazily. It only subscribes
+ * to them when at least one subscriber is present on the combined grain, and unsubscribes when
+ * the last subscriber is removed. This behavior optimizes resource usage by avoiding unnecessary
+ * updates when the combined grain is not being observed.
+ *
  * @template Dependencies - An array type representing the dependent ReadonlyGrains.
  * @template R - The type of value held by the resulting derived ReadonlyGrain.
  *
  * @param {Dependencies} deps - An array of ReadonlyGrains that the new grain depends on.
  * @param {(values: GrainValue<Dependencies>) => R} fn - A transformation function that takes the current values
  * of the dependent grains and returns the new value for the derived grain.
- * @param {R} [initial] - An optional initial value for the derived grain.
+ * @param {R} [initial] - An optional initial value for the derived grain. If not provided,
+ * the initial value is calculated using the transformation function `fn`.
  *
- * @returns {[ReadonlyGrain<R>, () => void]} A tuple containing the derived ReadonlyGrain and a
- * function to unsubscribe the derived grain from all dependent grains' changes.
+ * @returns {ReadonlyGrain<R>} A derived ReadonlyGrain that reflects the combined state of the dependent grains.
+ * The grain dynamically manages its subscriptions to dependent grains based on its own subscription state.
  *
  * @example
  * // Example of creating a combined ReadonlyGrain
  * const num1$ = grain(3);
  * const num2$ = grain(7);
- * const [sum$, unsubscribe] = combined([num1$, num2$], ([val1, val2]) => val1 + val2);
+ * const sum$ = combined([num1$, num2$], ([val1, val2]) => val1 + val2);
  *
  * // The combined grain reflects the sum of the values of num1$ and num2$
- * sum$.subscribe(sum => console.log(`Sum: ${sum}`));
+ * sum$.subscribe(sum => console.log(`Sum: ${sum}`)); // Logs: Sum: 10
  * num1$.set(5); // Updates sum$, logs: Sum: 12
  * num2$.set(10); // Updates sum$, logs: Sum: 15
+ *
+ * // When the last subscriber to sum$ is removed, sum$ unsubscribes from num1$ and num2$
  */
 
 export const combined = <Dependencies extends [ReadonlyGrain<any>, ...ReadonlyGrain<any>[]], R>(
     deps: Dependencies,
     fn: (values: GrainValue<Dependencies>) => R,
     initial?: R
-): [ReadonlyGrain<R>, () => void] => {
-    const _initial = initial ?? fn(deps.map((grain) => grain()) as GrainValue<Dependencies>);
-    const derived = grain(_initial);
+): ReadonlyGrain<R> => {
+    let unsubscriberCollection: (() => void)[] = [];
+    let subscribed = false;
+    const subscribers: Subscriber<R>[] = [];
 
-    let destroyed = false;
-    const _subs: (() => void)[] = [];
-    const destroy = () => {
-        if (!destroyed) {
-            _subs.forEach((sub) => sub());
-            destroyed = true;
+    const _initial = initial ?? fn(deps.map((grain) => grain()) as GrainValue<Dependencies>);
+    const combined = grain(_initial);
+    const subscriptionFunction = combined.subscribe;
+
+    const subscribe = () => {
+        if (subscribed) {
             return;
         }
+
+        subscribed = true;
+        unsubscriberCollection.push(
+            ...deps.map((grain) =>
+                grain.subscribe(() => combined.set(fn(deps.map((grain) => grain()) as GrainValue<Dependencies>)))
+            )
+        );
     };
 
-    deps.forEach((grain) => {
-        const invalidate = grain.subscribe(() =>
-            derived.set(fn(deps.map((grain) => grain()) as GrainValue<Dependencies>))
-        );
+    combined.subscribe = (subscriber: Subscriber<R>, seed = true) => {
+        if (!subscribed) {
+            subscribe();
+        }
 
-        _subs.push(invalidate);
-    });
+        subscribers.push(subscriber);
+        const innerUnsubscribe = subscriptionFunction(subscriber, seed);
 
-    return [readonly(derived), destroy];
+        return () => {
+            innerUnsubscribe();
+            const idx = subscribers.indexOf(subscriber);
+            if (idx !== -1) {
+                subscribers.splice(idx, 1);
+            }
+
+            // If no more subscriptions exist, destroy all subscriptions to the
+            // existing dependency grains
+            if (subscribers.length === 0 && unsubscriberCollection.length > 0) {
+                subscribed = false;
+                unsubscriberCollection.forEach((fn) => fn());
+                unsubscriberCollection = [];
+            }
+        };
+    };
+
+    return readonly(combined);
 };
--- src/lib/grains/derived.ts
+++ src/lib/grains/derived.ts
@@ -1,13 +1,18 @@
 /** @format */
 
+import { Subscriber } from '../../../dist/types';
 import { ReadonlyGrain } from '../../types';
 import { grain } from './grain';
 import { readonly } from './readonly';
-
 /**
  * Creates a new derived ReadonlyGrain from an existing grain by applying a transformation function.
  * This function allows you to create a new reactive state based on the value of another grain,
  * with the derived grain's value being a transformation of the original grain's value.
+ *
+ * The derived grain dynamically manages its subscription to the original grain. It subscribes to the
+ * original grain only when at least one subscriber is present on the derived grain and unsubscribes
+ * when the last subscriber is removed. This behavior optimizes resource usage by avoiding unnecessary
+ * updates when the derived grain is not being observed.
  *
  * @template T - The type of value held by the original ReadonlyGrain.
  * @template R - The type of value held by the derived ReadonlyGrain.
@@ -16,22 +21,60 @@ import { readonly } from './readonly';
  * @param {(value: T) => R} run - A transformation function to apply to the original grain's value.
  * It receives the original grain's value and returns the transformed value.
  *
- * @returns {[ReadonlyGrain<R>, () => void]} A tuple containing the derived ReadonlyGrain and a
- * function to unsubscribe the derived grain from the original grain's changes.
+ * @returns {ReadonlyGrain<R>} A derived ReadonlyGrain that reflects a transformed state of the original grain's value.
+ * The grain dynamically manages its subscription to the original grain based on its own subscription state.
  *
  * @example
  * // Example of creating a derived ReadonlyGrain
  * const number$ = grain(5); // Create a Grain with the value 5
- * const [isEven$, destroy] = derived(number$, n => n % 2 === 0); // Create a derived ReadonlyGrain
+ * const isEven$ = derived(number$, n => n % 2 === 0); // Create a derived ReadonlyGrain
  *
  * // The derived grain reflects whether the original grain's value is even
  * isEven$.subscribe(isEven => console.log(`Is number even? ${isEven}`));
  * number$.set(3); // Logs: Is number even? false
  * number$.set(4); // Logs: Is number even? true
+ *
+ * // When the last subscriber to isEven$ is removed, isEven$ unsubscribes from number$
  */
 
-export const derived = <T, R = T>(value: ReadonlyGrain<T>, run: (value: T) => R): [ReadonlyGrain<R>, () => void] => {
-    const _grain = grain<R>(run(value()));
-    const unsubscribe = value.subscribe((v) => _grain.set(run(v)));
-    return [readonly(_grain), () => unsubscribe()];
+export const derived = <T, R = T>(value: ReadonlyGrain<T>, run: (value: T) => R): ReadonlyGrain<R> => {
+    let unsubscriber: null | (() => void) = null;
+    let subscribed = false;
+    const subscribers: Subscriber<R>[] = [];
+
+    const initialValue = run(value());
+    const derived = grain<R>(initialValue);
+    const subscriptionFunction = derived.subscribe;
+
+    // Function to set up the subscription to the original grain
+    const subscribe = () => {
+        unsubscriber = value.subscribe((value) => derived.set(run(value)));
+        subscribed = true;
+    };
+    derived.subscribe = (subscriber: Subscriber<R>, seed = true) => {
+        if (!subscribed) {
+            subscribe();
+        }
+
+        subscribers.push(subscriber);
+        const innerUnsubscribe = subscriptionFunction(subscriber, seed);
+
+        return () => {
+            innerUnsubscribe();
+            const idx = subscribers.indexOf(subscriber);
+            if (idx !== -1) {
+                subscribers.splice(idx, 1);
+            }
+
+            // If no more subscriptions exist, destroy all subscriptions to the
+            // existing dependent grain
+            if (subscribers.length === 0 && unsubscriber) {
+                subscribed = false;
+                unsubscriber();
+                unsubscriber = null;
+            }
+        };
+    };
+
+    return readonly(derived);
 };
