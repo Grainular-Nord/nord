64293944e5d087047345b7a2cc1931947a3fd040
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-01-06 17:34:03 +0100

    feat: implement new each directive

--- src/lib/directives/each.ts
+++ src/lib/directives/each.ts
@@ -5,6 +5,12 @@ import { isElement } from '../../utils/is-element';
 import { isGrain } from '../../utils/is-grain';
 import { createDirective } from './create-directive';
 
+export type EachDirective<T> = {
+    as: (
+        runner: (value: T, index: number, arr: T[]) => NodeList
+    ) => Directive<Text> & { else: (nodes: NodeList) => Directive<Text> };
+};
+
 const øEqualizeNodeLists = (root: Element, list: Node[]) => {
     console.log({ root, list });
     // Normalize the root node
@@ -53,52 +59,79 @@ const øEqualizeNodeLists = (root: Element, list: Node[]) => {
  *
  * @param {T[] | ReadonlyGrain<T[]>} value - The array or grain containing the list of items to render.
  *   If it's a grain, the directive will reactively update the rendered list based on its current value.
- * @param {(elem: T, index: number) => NodeList} run - A function that returns a NodeList for each item in the array.
- *   This function is called for each item in the array, receiving the item and its index as arguments.
- *
- * @returns {Directive} An object representing the created template directive. This directive can be used within
+ * @returns {EachDirective<T>} An object representing the created template directive. This directive can be used within
  *   component templates to render a list of items based on the provided array or grain.
  *
  * @example
- * // Example of creating a list rendering directive using ForEach
- *
- * // The returned directive can be used in component templates to render a list of items.
- * <ul>${forEach(todos, (todo, index) =>
- *   html`<li key="${index}">${todo}</li>`
- * );}</ul>
- *
+ * // Example of creating a list rendering directive using Each
+ * ```ts
+ *  const entries = grain(['A', 'B', 'C']);
+ *  return html`<div>
+ *      ${each(entries)
+ *         .as((entry) => html`<div>${entry}</div>`)
+ *         .else(html`<div>No entries!</div>`)}
+ *      </div>`
+ * ```
  */
 
-export const forEach = <T>(
-    value: T[] | ReadonlyGrain<T[]>,
-    run: (elem: T, index: number) => NodeList
-): Directive<Text> => {
-    return createDirective(
-        (node: Text) => {
-            // Render the initial list
-            const initialValue: T[] = isGrain(value) ? value() : value;
-            let list = initialValue.flatMap((value, index) => [...run(value, index)]);
-            if (list.length === 0) {
-                list = [document.createTextNode('')];
+export const each = <T>(value: ReadonlyGrain<T[]> | T[]): EachDirective<T> => {
+    let elseNodes: Node[] = [document.createComment(`[Empty Else]`)];
+    let runner: ((value: T, index: number, arr: T[]) => NodeList) | null = null;
+
+    const getNodesFromValue = (value: T[]) => {
+        return value.flatMap((v, i, a) => [...runner!(v, i, a)]);
+    };
+
+    // Create the primary directive that is responsible for replacing the nodes
+    // once the data updates
+    const setContentDirective = createDirective<Text>(
+        (node) => {
+            // If no runner exists, abort;
+            if (!runner) {
+                return;
             }
 
+            // process the initial replacement
+            const initialValue: T[] = isGrain(value) ? value() : value;
+            let list = getNodesFromValue(initialValue);
+
+            // If the list is empty, show the fallback nodes instead.
+            if (list.length === 0) {
+                list = elseNodes;
+            }
+
+            // Append the nodes
             node.replaceWith(...list);
 
-            // If the value is a grain, subscriptions need to be setup to render the
-            // elements accordingly
+            // If the value is reactive, connect the nodes and the list to ensure
+            // Parity every time the value updates.
             if (isGrain(value)) {
-                // Get the common ancestor for all nodes. If there is more then one ancestor, something went very wrong
-                const [root, ...rest] = [...new Set(list.map((node) => node.parentElement))];
+                let current = list;
+                let [root, ...rest] = [...new Set(current.map((el) => el.parentElement))];
                 if (rest.length !== 0 || !root) throw new Error('[Nørd:Directive]: Directive has multiple roots.');
 
-                // Subscribe to the grain and equalize the nodeLists whenever the value changes
-                value.subscribe((value: T[]) => {
-                    const list = value.flatMap((value, index) => [...run(value, index)]);
-                    øEqualizeNodeLists(root, list);
+                // Subscribe to the grain
+                value.subscribe((value) => {
+                    const list = getNodesFromValue(value);
+                    øEqualizeNodeLists(root!, list);
                 });
-                return;
             }
         },
         { nodeType: 'Text' }
     );
+
+    // Add the `else` property to the directive to set the fallback nodes
+    Object.defineProperty(setContentDirective, 'else', {
+        value: (nodes: NodeList) => {
+            elseNodes = [...nodes];
+            return setContentDirective;
+        },
+    });
+
+    return {
+        as: (run: (value: T, index: number, arr: T[]) => NodeList) => {
+            runner = run;
+            return setContentDirective as Directive<Text> & { else: (nodes: NodeList) => Directive<Text> };
+        },
+    };
 };
