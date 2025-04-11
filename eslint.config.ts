eeeb851e9a3ceedc111ace1497ec7b66e3424208
Sebastian Heinz <sebastian@iamsebastian.dev>
2025-04-11 19:12:16 +0200

    chore: setup dx tools

--- eslint.config.ts
+++
@@ -1,9 +0,0 @@
-/** @format */
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config({
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.ts'],
-    ignores: ['node_modules/**', 'dist/**', '**/*.spec.ts', '**/*.d.ts'],
-});
