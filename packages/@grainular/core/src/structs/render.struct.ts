d68eea43a57e4ae307ef2248b71a529128a0b6e1
Sebastian Heinz <sebastian@iamsebastian.dev>
2025-04-14 09:50:05 +0200

    feat: add render struct to insert safe html

---
+++ packages/@grainular/core/src/structs/render.struct.ts
@@ -0,0 +1,13 @@
+import { Symbols } from '../internals/symbols';
+
+export const $render = (trustedHtml: string) => {
+    return Object.assign(
+        (root: Comment) => {
+            root.textContent += '[STRUCT@$render]';
+            const template = document.createElement('template');
+            template.innerHTML = trustedHtml;
+            root.after(template.content);
+        },
+        { [Symbols.STRUCT]: Symbols.STRUCT },
+    );
+};
