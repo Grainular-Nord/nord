f49c25f2c5ac7fc89a271b566ab55b34735b078b
Sebastian Heinz <sebastian@iamsebastian.dev>
2025-06-18 22:51:05 +0200

    docs: improve directive documentation

--- packages/@grainular/core/src/directives/on.directive.ts
+++ packages/@grainular/core/src/directives/on.directive.ts
@@ -1,5 +1,28 @@
 import { createDirective } from './create-directive';
 
+/**
+ * Attaches an event listener to a DOM element as a directive.
+ *
+ * This directive is used to declaratively bind DOM events to elements within templates.
+ * The listener will automatically be removed when the element is unmounted.
+ *
+ * @example
+ * ```ts
+ * import { on, html } from "@grainular/core";
+ *
+ * const clickHandler = (event: MouseEvent) => {
+ *     console.log("Clicked!", event);
+ * };
+ *
+ * const Button = () => html`
+ *   <button ${on("click", clickHandler)}>Click me</button>
+ * `;
+ * ```
+ *
+ * @param event - The event name to listen for (e.g., "click", "input").
+ * @param listener - The callback function to invoke when the event fires.
+ * @param options - Optional options for `addEventListener`.
+ */
 export const on = <Key extends keyof HTMLElementEventMap>(
     event: Key,
     listener: (event: HTMLElementEventMap[Key]) => void,
