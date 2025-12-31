22cd018129905421770616e0899079ccd038b3c2
Sebastian Heinz <sebastian@iamsebastian.dev>
2025-12-31 12:50:56 +0100

    feat: add dynamic render struct

---
+++ packages/@grainular/core/src/structs/render.struct.ts
@@ -0,0 +1,29 @@
+import { disconnectNodes } from '../application/lifecycle-observer';
+import type { ComponentFragment } from '../component/component-fragment';
+import { hydrateFragment } from '../internals/hydrate-fragment';
+import type { Subscribable } from '../internals/subscribable';
+import { createStruct } from './create-struct';
+
+export const $render = (source: Subscribable<(() => ComponentFragment) | ComponentFragment>) => {
+    return createStruct((node) => {
+        let nodes: Element[] = [];
+
+        const render = (maybeFragment: (() => ComponentFragment) | ComponentFragment) => {
+            disconnectNodes(nodes);
+
+            const fragment = typeof maybeFragment === 'function' ? maybeFragment() : maybeFragment;
+            nodes = hydrateFragment(fragment);
+
+            node.before(...nodes);
+        };
+
+        render(source());
+        const ondestroy = source.subscribe(render);
+
+        return () => {
+            ondestroy?.();
+            disconnectNodes(nodes);
+            nodes = [];
+        };
+    });
+};
