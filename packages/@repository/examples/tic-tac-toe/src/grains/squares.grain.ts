4bc722ba61de710a1ae9c85a96e4fc65b5d2ed36
Sebastian Heinz <sebastian@iamsebastian.dev>
2025-12-31 14:05:29 +0100

    chore: improve repository structure

--- packages/@repository/examples/tic-tac-toe/src/grains/squares.grain.ts
+++
@@ -1,8 +0,0 @@
-import { grain } from '@grainular/grains';
-
-export type PlayerSymbol = 'X' | 'O';
-
-export const squares = [...Array.from({ length: 9 }, () => grain<PlayerSymbol | null>(null))];
-export const resetSquares = () => {
-    for (const square of squares) square.set(null);
-};
--- packages/@repository/examples/tic-tac-toe/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { mount } from '@grainular/core';
-import { App } from './app';
-
-mount(App, { to: document.querySelector('#app') });
