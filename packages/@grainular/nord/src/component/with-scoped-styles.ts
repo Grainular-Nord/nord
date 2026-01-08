bb057c02c869dbd476b426618807ad12b32a13c0
Sebastian Heinz <sebastian@iamsebastian.dev>
2026-01-08 21:46:54 +0100

    fix: resolve issues with scoped styles typings

--- packages/@grainular/nord/src/component/with-scoped-styles.ts
+++ packages/@grainular/nord/src/component/with-scoped-styles.ts
@@ -1,11 +1,14 @@
 import type { StyleFragment } from '../application/style-parser';
 import { type ComponentFragment, IS_COMPONENT } from './component-fragment';
-import type { PureComponent } from './component-types';
+import type { ComponentProps } from './component-types';
 
-export const withScopedStyles = <T extends PureComponent>(component: T, styles: StyleFragment) => {
-    return (...args: Parameters<T>): ComponentFragment => {
-        // @ts-expect-error: We can safely pass arguments even to a component that
-        // doesn't want them. If no arguments are passed, undefined will be correct
+export const withScopedStyles = <P extends ComponentProps | undefined = undefined>(
+    component: (args: P) => ComponentFragment,
+    styles: StyleFragment,
+) => {
+    return (args: P = undefined as P): ComponentFragment => {
+        // We pass the argument here even if it's undefined,
+        // which is fine for a component without props
         const fragment = component(args);
 
         return {
--- packages/@grainular/nord/src/internals/trim-whitespace.ts
+++ packages/@grainular/nord/src/internals/trim-whitespace.ts
@@ -1,3 +1,3 @@
 export const trimWhitespace = (str: string) => {
-    return str.replace(/\s{2,}/g, ' ').trim();
+    return str.replace(/\s{2,}/g, ' ');
 };
