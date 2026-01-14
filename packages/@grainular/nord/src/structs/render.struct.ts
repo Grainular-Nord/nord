aebc4dc8367d1b21b854f1533cc426fc0d7c39aa
Sebastian Heinz <sebastian@iamsebastian.dev>
2026-01-14 20:46:38 +0100

    fix: add ssr render to render struct

--- packages/@grainular/nord/src/structs/render.struct.ts
+++ packages/@grainular/nord/src/structs/render.struct.ts
@@ -4,26 +4,31 @@ import { hydrateFragment } from '../internals/hydrate-fragment';
 import type { Subscribable } from '../internals/subscribable';
 import { createStruct } from './create-struct';
 
-export const $render = (source: Subscribable<(() => ComponentFragment) | ComponentFragment>) => {
-    return createStruct((node) => {
-        let nodes: Element[] = [];
+export const $render = (source: Subscribable<ComponentFragment | ComponentFragment>) => {
+    return createStruct(
+        (node) => {
+            let nodes: Element[] = [];
 
-        const render = (maybeFragment: (() => ComponentFragment) | ComponentFragment) => {
-            disconnectNodes(nodes);
+            const render = (maybeFragment: (() => ComponentFragment) | ComponentFragment) => {
+                disconnectNodes(nodes);
 
-            const fragment = typeof maybeFragment === 'function' ? maybeFragment() : maybeFragment;
-            nodes = hydrateFragment(fragment);
+                const fragment = typeof maybeFragment === 'function' ? maybeFragment() : maybeFragment;
+                nodes = hydrateFragment(fragment);
 
-            node.before(...nodes);
-        };
+                node.before(...nodes);
+            };
 
-        render(source());
-        const ondestroy = source.subscribe(render);
+            render(source());
+            const ondestroy = source.subscribe(render);
 
-        return () => {
-            ondestroy?.();
-            disconnectNodes(nodes);
-            nodes = [];
-        };
-    });
+            return () => {
+                ondestroy?.();
+                disconnectNodes(nodes);
+                nodes = [];
+            };
+        },
+        () => {
+            return source().render();
+        },
+    );
 };
