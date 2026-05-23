e33e318221a59bff03a274c28618612043031622
Sebastian Heinz <sebastian@iamsebastian.dev>
2026-05-23 18:02:28 +0200

    refactor: clean up sep of concerns

--- examples/game-of-life/src/components/controls/controls.component.ts
+++ examples/game-of-life/src/components/controls/controls.component.ts
@@ -5,13 +5,14 @@ import './controls.component.css';
 
 export const Controls = () => {
     const { state, actions } = simulationState;
-    const { running, generation } = state;
+    const { running, generation, stash } = state;
 
     return html`
         <div class="controls">
             <button data-active="${derived(running, (r) => !r)}" ${on('click', () => actions.stopSimulation())}>⏸️</button>
             <button data-active="${running}" ${on('click', () => actions.startSimulation())}>▶️</button>
-            <button ${on('click', () => actions.nextSimulationStep())}>⏭️</button>
+            <button ${on('click', () => actions.nextSimulationStep())}>⏩️</button>
+            <button disabled="${derived(stash, ({ length }) => length <= 0)}" ${on('click', () => actions.previousSimulationStep())}>⏪️</button>
             <button ${on('click', () => actions.resetSimulation())}>🔄</button>
             <button disabled="${derived(generation, (gen) => gen !== 0)}" ${on('click', () => actions.randomizeCells())}>#️⃣</button>    
         </div>
--- examples/game-of-life/src/stores/simulation-state.ts
+++ examples/game-of-life/src/stores/simulation-state.ts
@@ -1,8 +1,9 @@
 import { type WritableGrain, grain } from '@grainular/grains';
 
-export const rows = 200;
-export const cols = 200;
-export const gensPerSecond = 25;
+const rows = 50;
+const cols = 50;
+const gensPerSecond = 25;
+const maxStashEntries = 50;
 
 const cells: WritableGrain<boolean>[][] = Array.from({ length: cols }, () => {
     return Array.from({ length: rows }, () => {
@@ -10,87 +11,127 @@ const cells: WritableGrain<boolean>[][] = Array.from({ length: cols }, () => {
     });
 });
 
-const running = grain(false);
-const generation = grain(0);
-
 // alive is maintained as a write-through cache rather than derived
 // from cells directly. Using combined(cells.flat()) tanks performance
 // at >10k cells. All mutations to cells must update alive accordingly.
 const alive = grain(0);
+const running = grain(false);
+const generation = grain(0);
+const stash = grain<boolean[][][]>([]);
 
-const updateAlive = (amount: number) => {
-    alive.set(alive() + amount);
+// -- Internal State Processors
+const updateAliveCells = (amount: number) => alive.set(alive() + amount);
+
+// Convenience Helper to iterate all cells
+// using a callback fn provided
+const iterateCells = (itt: (cell: WritableGrain<boolean>, pos: [x: number, y: number]) => void) => {
+    cells.forEach((row, idx) => {
+        row.forEach((cell, idy) => {
+            itt(cell, [idx, idy]);
+        });
+    });
 };
 
-const getCellNeighbors = (cellId: [x: number, y: number]) => {
-    const [x, y] = cellId;
-    // Neighbors are available here:
-    // -1/-1    0/-1    +1/-1
-    // -1/0     O/0     +1/0
-    // -1/+1    0/+1    +1/+1
-
-    return [
-        // Above
-        cells[x - 1]?.[y - 1],
-        cells[x]?.[y - 1],
-        cells[x + 1]?.[y - 1],
-        // Center
-        cells[x - 1]?.[y],
-        cells[x + 1]?.[y],
-        // Below
-        cells[x - 1]?.[y + 1],
-        cells[x]?.[y + 1],
-        cells[x + 1]?.[y + 1],
-    ];
+// Convenience Helper to retrieve all
+// neighbors of a given cell.
+const getCellNeighbors = ([x, y]: [x: number, y: number]) => {
+    const indices = [-1, 0, 1];
+    return indices.flatMap((idx) =>
+        indices.map((idy) => {
+            // Skip self
+            if (idx === 0 && idy === 0) return;
+            return cells[x + idx]?.[y + idy];
+        }),
+    );
 };
 
+// Calculates the next state of a cell based
+// on it's current state and neighbors
 const getNextCellState = (state: WritableGrain<boolean>, cellId: [x: number, y: number]) => {
     const neighbors = getCellNeighbors(cellId);
-    const alive = neighbors.filter((neighbor) => neighbor?.());
+    const aliveNeighbors = neighbors.filter((neighbor) => neighbor?.());
 
     if (state()) {
         // Rule 1, 2, & 3: A live cell only survives if it has 2 or 3 neighbors.
-        // Otherwise, it dies (underpopulation or overpopulation).
-        return alive.length === 2 || alive.length === 3;
+        // Otherwise, it dies (under-population or overpopulation).
+        return aliveNeighbors.length === 2 || aliveNeighbors.length === 3;
     }
 
     // Rule 4: A dead cell only comes to life if it has exactly 3 neighbors.
     // Otherwise, it stays dead.
-    return alive.length === 3;
+    return aliveNeighbors.length === 3;
 };
 
-const simulationStep = (manual = false) => {
-    // Abort if simulation doesn't run
-    if (!running() && manual === false) return;
+const nextSimulationStep = () => {
+    generation.set(generation() + 1);
+    // Stash the state as the latest snapshot, and
+    // truncate if longer then maxStashEntries
+    stash.update((current) => {
+        const nextStash = [...current, cells.map((row) => row.map((cell) => cell()))];
+        if (nextStash.length > maxStashEntries) nextStash.shift();
+        return nextStash;
+    });
 
-    // Find next cell state
-
+    // Find next cell state by applying
+    // the game rules against the cell
     const nextBatch = cells.map((col, idx) => {
         return col.map((cell, idy) => {
             return getNextCellState(cell, [idx, idy]);
         });
     });
 
-    // Apply rules
+    // Apply state while tracking the number
+    // of alive cells remaining.
     let trackAlive = 0;
-    cells.forEach((row, idx) => {
-        row.forEach((cell, idy) => {
-            cell.set(nextBatch[idx][idy]);
-            if (nextBatch[idx][idy]) trackAlive++;
-        });
+    iterateCells((cell, [x, y]) => {
+        cell.set(nextBatch[x][y]);
+        nextBatch[x][y] && trackAlive++;
     });
-    alive.set(trackAlive);
 
-    generation.update((current) => current + 1);
-    window.setTimeout(() => window.requestAnimationFrame(() => simulationStep()), 1000 / gensPerSecond);
+    // We set the alive cell count once to avoid unnecessary updates.
+    // This means that the alive counter lags behind
+    // the actual alive cells (as they are intermittently set)
+    // but is up to date enough to be useful
+    alive.set(trackAlive);
+};
+
+const previousSimulationStep = () => {
+    running.set(false);
+    generation.set(generation() - 1);
+
+    // Retrieve the latest entry and keep track
+    // of the rest of the elements to update the stash
+    // accordingly
+    const [lastBatch, ...rest] = stash().toReversed();
+    if (!lastBatch) return;
+
+    let trackAlive = 0;
+    iterateCells((cell, [x, y]) => {
+        cell.set(lastBatch[x][y]);
+        lastBatch[x][y] && trackAlive++;
+    });
+
+    alive.set(trackAlive);
+    stash.set(rest.toReversed());
+};
+
+const stepThroughSimulation = () => {
+    if (!running()) return;
+
+    nextSimulationStep();
+
+    window.setTimeout(() => {
+        window.requestAnimationFrame(() => {
+            stepThroughSimulation();
+        });
+    }, 1000 / gensPerSecond);
 };
 
 // Setup and start the simulation.
 // Returns a cleanup function that stops any
 // further simulation steps
 const runSimulation = () => {
-    window.requestAnimationFrame(() => simulationStep());
-
+    window.requestAnimationFrame(() => stepThroughSimulation());
     return () => running.set(false);
 };
 
@@ -98,12 +139,7 @@ const runSimulation = () => {
 const toggleCell = (state: WritableGrain<boolean>) => {
     const next = !state();
     state.set(next);
-    updateAlive(next ? 1 : -1);
-};
-
-const nextSimulationStep = () => {
-    running.set(false);
-    simulationStep(true);
+    updateAliveCells(next ? 1 : -1);
 };
 
 const startSimulation = () => {
@@ -118,14 +154,11 @@ const stopSimulation = () => {
 };
 
 const resetSimulation = () => {
-    stopSimulation();
-    for (const row of cells) {
-        for (const cell of row) {
-            cell.set(false);
-        }
-    }
+    running.set(false);
     alive.set(0);
     generation.set(0);
+    stash.set([]);
+    iterateCells((cell) => cell.set(false));
 };
 
 const randomizeCells = () => {
@@ -146,10 +179,11 @@ export const simulationState = {
         resetSimulation,
         startSimulation,
         nextSimulationStep,
+        previousSimulationStep,
         stopSimulation,
         randomizeCells,
         toggleCell,
     },
     parameters: { rows, cols, gensPerSecond },
-    state: { cells, running, generation, alive },
+    state: { cells, running, generation, alive, stash },
 };
