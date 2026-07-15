4e96198a2ae6b22bb43ab4a1becf43c8c21c1345
Sebastian Heinz <sebastian@iamsebastian.dev>
2026-07-15 23:19:54 +0200

    chore: remove outdated code

--- sites/playground.nordjs.dev/src/tip.ts
+++
@@ -1,5 +0,0 @@
-import { type PropsWithChildren, html } from '@grainular/nord';
-
-export const Tip = ({ children }: PropsWithChildren) => {
-    return html`<div style="background: gray; border: 1px solid darkgray; padding-inline: 1rem;">${children}</div>`;
-};
--- sites/playground.nordjs.dev/src/vite-env.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.md' {
-    export const meta: { title?: string } & Record<string, unknown>;
-    export const content: string;
-}
